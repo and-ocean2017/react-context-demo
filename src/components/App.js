d27b253fb9da4f68201b34a6fb0adc5354a720f0
and-ocean2017 <and.ocean2017@gmail.com>
2021-01-26 23:37:39 +0530

    created different context and consumer component

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import { Component } from 'react';
 import Main from './Main';
+import ThemeContext from '../context/ThemeContext';
+import ColorContext from "../context/ColorContext";
 
 class App extends Component {
   state = {
@@ -16,17 +18,23 @@ class App extends Component {
 
   render() {
       return (
-          <div className="p-4 h-100 d-flex flex-column">
-            <div className="d-flex mb-4 justify-content-center align-items-center">
-              <button
-                className="btn btn-sm btn-warning"
-                onClick={this.toggleTheme}
-              >
-                toggle {this.state.theme} theme
-              </button>
+        <ThemeContext.Provider value={this.state.theme}>
+          <ColorContext.Provider value={'red'}>
+                  
+            <div className="p-4 h-100 d-flex flex-column">
+              <div className="d-flex mb-4 justify-content-center align-items-center">
+                <button
+                  className="btn btn-sm btn-warning"
+                  onClick={this.toggleTheme}
+                >
+                  toggle {this.state.theme} theme
+                </button>
+              </div>
+              <Main />
             </div>
-            <Main />
-          </div>
+                  
+          </ColorContext.Provider>
+        </ThemeContext.Provider>
       );
   }
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,25 +1,34 @@
 import React, { Component } from 'react';
 import ThemeContext from '../context/ThemeContext';
+import ColorContext from '../context/ColorContext';
 class Main extends Component {
-  mainStyle = () => {
+  mainStyle = (theme) => {
     return {
-      backgroundColor: this.context === "light" ? "white" : "black",
-      color: this.context === "light" ? "black" : "white",
+      backgroundColor: theme === "light" ? "white" : "black",
+      color: theme === "light" ? "black" : "white",
     };
   };
     
   render() {
       return (
-        <ThemeContext.Consumer>
+          <ThemeContext.Consumer>
           {
             (theme) => {
                 return (
-                <div
+                  <div
                     className="h-100 border rounded border-secondary p-4 d-flex justify-content-center align-items-center"
-                    style={this.mainStyle()}
-                >
-                    <h4>Current theme is: { theme }</h4>
-                </div>
+                    style={this.mainStyle(theme)}
+                  >
+                    <ColorContext.Consumer>
+                      {(color) => {
+                        return (
+                          <h4 style={{ color: color }}>
+                            Current theme is: {theme}
+                          </h4>
+                        );
+                      }}
+                    </ColorContext.Consumer>
+                  </div>
                 );
             }
           }
